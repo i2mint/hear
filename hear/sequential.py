ecb2bcf50011674e05fd3e9fe8b2e84fb894417e
Thor Whalen <thorwhalen1@gmail.com>
2021-04-06 17:28:29 -0700

    just rerouting some references to mk_regular_panel_data_rw_funcs

--- hear/sequential.py
+++ hear/sequential.py
@@ -1,5 +1,5 @@
 """A place to find the main functions for serialization of sequential data (time series, panel data, streams, etc.)"""
 
-from py2store.serializers.regular_panel_data import (
+from hear.regular_panel_data import (
     mk_reader_and_writer as mk_regular_panel_data_rw_funcs,
 )
