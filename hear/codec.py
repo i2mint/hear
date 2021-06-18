c5f1b48fb024974f9a1f06cf1a45f96ceecd06c4
Thor Whalen <thorwhalen1@gmail.com>
2021-06-18 09:55:42 -0700

    changed codec and made doctests

--- hear/codec.py
+++ hear/codec.py
@@ -41,7 +41,7 @@ first_element = itemgetter(0)
 class ChunkedDecoder(Decoder):
     chk_size_bytes: int  # positive integer in fact TODO: builtin type for that?
     chk_to_frame: ChunkToFrame
-    single_channel: bool = False
+    n_channels: int = None
 
     # ByteChunker
     def chunker(self, b: bytes) -> Chunks:
@@ -50,7 +50,7 @@ class ChunkedDecoder(Decoder):
 
     def __call__(self, b: bytes):
         frames = map(self.chk_to_frame, self.chunker(b))
-        if self.single_channel:
+        if self.n_channels:
             return map(first_element, frames)
         return frames
 
@@ -58,22 +58,69 @@ class ChunkedDecoder(Decoder):
 ChkFormat = str  # a str recognized as a struct format
 
 
+def _chk_format_is_for_single_channel(chk_format):
+    """Checks if a chk_format string is meant for a single channel codec"""
+    if chk_format[0] in "@=<>!":
+        chk_format = chk_format[1:]  # remove the byte order character before checking
+    return len(chk_format) == 1
+
+
 @dataclass
 class StructCodecSpecs:
-    """
+    r"""
     :param chk_format: The format of a chunk, as specified by the struct module
         See https://docs.python.org/3/library/struct.html#format-characters
     :param n_channels: Only n_channels = 1 serves a purpose; to indicate that
+
+    >>> specs = StructCodecSpecs(
+    ...     chk_format='h',
+    ... )
+    >>> print(specs)
+    StructCodecSpecs(chk_format='h', n_channels=1, chk_size_bytes=2)
+    >>>
+    >>> encoder = ChunkedEncoder(
+    ...     frame_to_chk=specs.frame_to_chk
+    ... )
+    >>> decoder = ChunkedDecoder(
+    ...     chk_size_bytes=specs.chk_size_bytes,
+    ...     chk_to_frame=specs.chk_to_frame,
+    ...     n_channels=specs.n_channels
+    ... )
+    >>>
+    >>> frames = [1, 2, 3]
+    >>> b = encoder(frames)
+    >>> assert b == b'\x01\x00\x02\x00\x03\x00'
+    >>> decoded_frames = list(decoder(b))
+    >>> assert decoded_frames == frames
     """
 
     chk_format: str
-    single_channel: bool = True
+    n_channels: int = None
+    chk_size_bytes: int = None
 
     def __post_init__(self):
-        self.chk_size_bytes = struct.calcsize(self.chk_format)
+        if self.n_channels is None:
+            self.n_channels = len(self.chk_format)
+        else:
+            assert _chk_format_is_for_single_channel(self.chk_format), (
+                    "if n_channels is given, chk_format needs to be for a single channel"
+                )
+            self.chk_format = self.chk_format * self.n_channels
+
+        chk_size_bytes = struct.calcsize(self.chk_format)
+        if self.chk_size_bytes is None:
+            self.chk_size_bytes = chk_size_bytes
+        else:
+            assert self.chk_size_bytes == chk_size_bytes, (
+                f"The given chk_size_bytes {self.chk_size_bytes} did not match the "
+                f"inferred (from chk_format) {chk_size_bytes}"
+            )
 
     def frame_to_chk(self, frame):
-        return pack(self.chk_format, frame)
+        if self.n_channels == 1:
+            return pack(self.chk_format, frame)
+        else:
+            return pack(self.chk_format, *frame)
 
     def chk_to_frame(self, chk):
         return unpack(self.chk_format, chk)
